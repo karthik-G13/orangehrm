a8dedeb82705e6a137b84a0852c8fcd4beb68c0b
karthik.G13 <donkarthik2555@gmail.com>
2026-08-08 23:37:42 +0530

    delete

--- framework/tests/automation.spec.js
+++
@@ -1,8 +0,0 @@
-import {test,expect} from "../fixtures/orangeHRM";
-
-
-
-test('fixture',async({loggedInPage,dashboard})=>{
-    await loggedInPage.login(process.env.Username,process.env.Password);
-    await dashboard.isDashboardVisible();
-})
